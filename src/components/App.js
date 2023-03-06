8a822028c8d9dca7a5a37e99530367c2696a7314
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-06 14:36:21 +0900

    [추가] 카피라이트 하나 추가해봤음

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import Router from "./Router";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  return <Router isLoggedIn={isLoggedIn} />;
+  return (
+    <>
+      <Router isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
 }
 
 export default App;

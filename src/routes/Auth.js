4440a7026f158170a634c4c8ab2ed17d238d4889
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-06 16:13:03 +0900

    [추가] 간단히 form 을 컨틀로 할수 있는 방법

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,40 @@
+import { useState } from "react";
+
 export default function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Email" required />
-        <input type="password" placeholder="password" required />
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          value={password}
+          onChange={onChange}
+        />
         <input type="submit" value="Log In" />
       </form>
       <div>

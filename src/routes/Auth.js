64bc4af1bc04cdb5bb4ce26158a424ef29ba6879
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-06 16:03:57 +0900

    [추가] 로그인 폼 jsx 추가

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,15 @@
 export default function Auth() {
-  return <div>Auth</div>;
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Email" required />
+        <input type="password" placeholder="password" required />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with GitHub</button>
+      </div>
+    </div>
+  );
 }

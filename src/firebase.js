c348384fdfcae843a80ba18e0994505b946cf59b
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-06 13:13:30 +0900

    [추가] firebase 환경 설정

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZAyD2zRy5vuatzTRoISHBWvD1GB6Pb6o",
+  authDomain: "nwitter-e33e5.firebaseapp.com",
+  projectId: "nwitter-e33e5",
+  storageBucket: "nwitter-e33e5.appspot.com",
+  messagingSenderId: "441802188520",
+  appId: "1:441802188520:web:780ba3464d82e231a18b5d",
+};
+
+export default firebase.initializeApp(firebaseConfig);

1ea818fd313a46847804f78472c6d88ab9926d00
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-12 16:07:43 +0900

    [추가] value를 setNweet를 이용해서 변경해주기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,11 +5,22 @@ export default function Home() {
   const onSubmit = (e) => {
     e.preventDefault();
   };
-  const onChange = (e) => {};
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNweet(value);
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
         <input type="submit" value="Nweet" />
       </form>
     </div>

fb96984e3e3515069493f62592b69ef56c072085
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-12 16:03:26 +0900

    [추가] onSubmit 함수 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,10 @@
 export default function Home() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <input type="text" placeholder="What's on your mind?" maxLength={120} />
         <input type="submit" value="Nweet" />
       </form>

494d763aeadc34bb50e2a802bfc27ffc8c5e0c8f
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-12 16:01:36 +0900

    [추가] 서브밋 폼 완성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
 }

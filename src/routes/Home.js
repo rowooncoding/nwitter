ac98cfae186e980ab4a96529d248cf27b1b36914
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-12 18:31:31 +0900

    [추가] 내가 작성한 nweet 화면에 뿌려주기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,7 +9,13 @@ export default function Home() {
   const [nweets, setNweets] = useState([]);
   const getNweets = async () => {
     const dbNweets = await getDocs(dbCollection(dbService, "nweets"));
-    dbNweets.forEach((document) => console.log(document.data()));
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
   };
   useEffect(() => {
     getNweets();
@@ -40,6 +46,13 @@ export default function Home() {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
